9bec92f19461a1ec6f4f426293a94014c042ad3e
Bhawak Pokhrel <bhawak.pokhrel@gmail.com>
2020-08-31 12:54:40 +0545

    Initial Commit

---
+++ BaatoSwift/BaatoSwift.h
@@ -0,0 +1,19 @@
+//
+//  BaatoSwift.h
+//  BaatoSwift
+//
+//  Created by Bhawak Pokhrel on 8/31/20.
+//  Copyright © 2020 Bhawak Pokhrel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaatoSwift.
+FOUNDATION_EXPORT double BaatoSwiftVersionNumber;
+
+//! Project version string for BaatoSwift.
+FOUNDATION_EXPORT const unsigned char BaatoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaatoSwift/PublicHeader.h>
+
+
